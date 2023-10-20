e101eb4e7d8d24ee1ad395165768bc5223ec6f90
Willbeasti <139876491+willbeasti@users.noreply.github.com>
2023-10-20 12:14:05 +0300

    Revert "Merge pull request #2 from willbeasti/release"
    This reverts commit 6960d5e6f5fddfbc848582ee35c5b4b193db1865, reversing
    changes made to 82a123999312759e8bdfd7714af149f5d55efed3.

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,10 @@
 <<<<<<< HEAD
 
 def main():
-  print("Hello from release")
+  print("Hello from feature")
 
 if __name__ == "__main__":
   main()
 =======
-print("Hello from release")
+print("Hello from feature")
 >>>>>>> keskitaso/master
